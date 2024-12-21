13b762469dae4ffd7fd89b39973adae8f7cef6f5
Edil Ormanaliyev <157285886+MrVilka@users.noreply.github.com>
2024-12-21 21:18:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function myf1() {
+  window.open("kran-sh.html");
+}
+function myf3() {
+  window.open('zadt.html')
+}
+function myf2() {
+  window.open('flan.html')
+}
+function myf4() {
+  window.open('det.html')
+}
+function myf5() {
+  window.open('proche.html')
+}
+
+let burger = document.querySelector('.burger');
+let nav = document.querySelector(".nav");
+burger.addEventListener('click', function(){
+  if(window.innerWidth < 701){
+    this.classList.toggle('active');
+    nav.classList.toggle('open');
+  }
+});
